4fbe6a204c644ed377c798c0f101f3f0780f3cb9
zwh <zwh18123@sina.com>
2016-07-06 23:00:42 +0800

    first 提交

---
+++ cal.py
@@ -0,0 +1,7 @@
+class Count:
+    def __init__(self,a,b):
+        self.a=int(a)
+        self.b=int(b)
+
+    def  add(self):
+        return  self.a+self.b
---
+++ test.py
@@ -0,0 +1,25 @@
+from cal import Count
+import unittest
+
+class TestCount( unittest.TestCase):
+    def setUp(self):
+        print ( "test  start")
+
+    def test_add(self):
+        j=Count (2,3)
+        self.assertEqual(j.add(),7)
+
+    def test_add2(self):
+        j = Count(23, 33)
+        self.assertEqual(j.add(), 56)
+
+    def tearDown(self):
+        print ("test  end")
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    suite.addTest(TestCount("test_add"))
+    suite.addTest(TestCount("test_add2"))
+    runner == unittest.TextTestRunner()
+    runner.run(suite)
+    unittest.main()
